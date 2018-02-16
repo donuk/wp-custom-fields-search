1f2933cab3fa22ddff0ca7e8452b23885ffab445
Don Benjamin <don@webhammer.co.uk>
2018-02-16 14:01:56 +0000

    taiga-11 Expanding JS Test Suite

--- tests/js/controller-tests.js
+++ tests/js/controller-tests.js
@@ -40,12 +40,33 @@ describe('controllers', function(){
             expect($scope.popped_up_field).toBe("test");
         });
 
+        it('test_remove_field',function(){
+            createController("WPCFSForm",{"$scope": $scope, "i18n": mock_i18n});
+
+            $scope.form_fields = [ "a","b","c" ];
+            $scope.remove_field("b");
+            expect($scope.form_fields).toEqual(["a","c"]);
+
+            $scope.form_fields = [ "a","c" ];
+            $scope.remove_field("b");
+            expect($scope.form_fields).toEqual(["a","c"]);
+        });
+
         it('test_close_edit_form',function(){
             createController("WPCFSForm",{"$scope": $scope, "i18n": mock_i18n});
 
             $scope.popped_up_field="test";
             $scope.close_edit_form();
             expect($scope.popped_up_field).toBe(null);
+        });
+
+        it('test_show_settings_popup',function(){
+            createController("WPCFSForm",{"$scope": $scope, "i18n": mock_i18n});
+            expect($scope.settings_visible).toBe(false);
+            $scope.show_settings_popup();
+            expect($scope.settings_visible).toBe(true);
+            $scope.close_settings_popup();
+            expect($scope.settings_visible).toBe(false);
         });
     });
     
