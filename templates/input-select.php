4a3bfd7333178801353bf24bd7705d680fd5af36
Don Benjamin <don@webhammer.co.uk>
2021-07-20 11:25:00 +0100

    #taiga-33 Suppress warning messages on input template

--- templates/input-select.php
+++ templates/input-select.php
@@ -2,7 +2,8 @@
 
 <?php foreach($options['options'] as $option) {
 ?>
-	<option value="<?php echo htmlspecialchars($option['value']); ?>" <?php if($option['value']==$query[$html_name]) { ?> selected='selected'<?php } ?> >
+	<option value="<?php echo htmlspecialchars($option['value']); ?>"
+		 <?php if(array_key_exists($html_name, $query) && ($option['value']==$query[$html_name])) { ?> selected='selected'<?php } ?> >
 		<?php echo htmlspecialchars($option['label']);?>
 	</option>
 <?php } ?>
